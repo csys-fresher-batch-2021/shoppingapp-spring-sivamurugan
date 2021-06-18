1247987585faa0ed2ead6bfbfff4b683e3bcc6bd
Siva Murugan B <sivamurugan5101999@gmail.com>
2021-06-18 17:20:38 +0530

    fixed a small mistake

--- src/main/java/in/siva/vegapp/controller/UserController.java
+++ src/main/java/in/siva/vegapp/controller/UserController.java
@@ -8,18 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import in.siva.vegapp.dao.UserRepository;
 import in.siva.vegapp.model.UserDetail;
+import in.siva.vegapp.service.UserService;
 
 @RestController
 @RequestMapping("vegapp/v1/users")
 public class UserController {
 	@Autowired
 	UserRepository userRepo;
+	@Autowired
+	UserService userService;
 	
 	@PostMapping("register")
 	public String register(@RequestBody UserDetail user) {
 		String message = "Successfully registered";
 		try {
-			userRepo.save(user);
+			userService.registerUser(user);
 		} catch(Exception e) {
 			message = "Registration failed";
 		}
