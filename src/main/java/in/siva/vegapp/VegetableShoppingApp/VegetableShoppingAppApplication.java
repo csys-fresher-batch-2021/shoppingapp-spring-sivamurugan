c7ed4e90edc841e848d7fb254cccefc43a968525
Siva Murugan B <sivamurugan5101999@gmail.com>
2021-06-18 12:14:47 +0530

    initial commit

---
+++ src/main/java/in/siva/vegapp/VegetableShoppingApp/VegetableShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package in.siva.vegapp.VegetableShoppingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetableShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegetableShoppingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/siva/vegapp/VegetableShoppingApp/VegetableShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.siva.vegapp.VegetableShoppingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegetableShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
