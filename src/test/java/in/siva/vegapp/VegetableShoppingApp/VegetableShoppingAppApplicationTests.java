6425ba2e2bfd418e53c3fc6419e5ad58b4b1febe
Siva Murugan B <sivamurugan5101999@gmail.com>
2021-06-26 10:56:32 +0530

    small fix

--- src/test/java/in/siva/vegapp/VegetableShoppingApp/VegetableShoppingAppApplicationTests.java
+++ src/test/java/in/siva/vegapp/VegetableShoppingApp/VegetableShoppingAppApplicationTests.java
@@ -1,27 +1,27 @@
-//package in.siva.vegapp.VegetableShoppingApp;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import in.siva.vegapp.dao.UserRepository;
-//
-//@SpringBootTest
-//class VegetableShoppingAppApplicationTests {
-//
-//	@Autowired
-//	UserRepository userRepo;
-//
-//	@Test
-//	void loginTest() {
-//		String findRoleOfUser = userRepo.findRoleOfUser("siva123", "Siva123@");
-//		System.out.println(findRoleOfUser);
-//	}
-//
-//	@Test
-//	void updateNameTest() {
-//		Long id = userRepo.updateNameById("shanthi", 2l);
-//		System.out.println(id);
-//	}
-//
-//}
+package in.siva.vegapp.VegetableShoppingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import in.siva.vegapp.dao.UserRepository;
+
+@SpringBootTest
+class VegetableShoppingAppApplicationTests {
+
+	@Autowired
+	UserRepository userRepo;
+
+	@Test
+	void loginTest() {
+		String findRoleOfUser = userRepo.findRoleOfUser("siva123", "Siva123@");
+		System.out.println(findRoleOfUser);
+	}
+
+	@Test
+	void updateNameTest() {
+		Long id = userRepo.updateNameById("shanthi", 2l);
+		System.out.println(id);
+	}
+
+}
